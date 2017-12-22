7d7c3ffae06989fe562057b329c919a2c5e54ebc
EthanVG <ethanvascellaro@gmail.com>
2017-12-22 03:33:34 +0000

    Travis File

--- bulletJournal/config.py
+++ bulletJournal/config.py
@@ -6,3 +6,7 @@ class DevelopmentConfig(object):
 class TestingConfig(object):
     DATABASE_URI="postgresql://ubuntu:thinkful@localhost:5432/bulletJournal-test"
     DEBUG = True
+    
+class TravisConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/bulletJournal-test"
+    DEBUG = False
